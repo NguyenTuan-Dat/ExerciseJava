4000413530da2c460d144fdae34563286392da18
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-15 16:00:41 +0700

    Update

--- src/main/java/PTThuaSoNguyenTo.java
+++ src/main/java/PTThuaSoNguyenTo.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class PTThuaSoNguyenTo {
+    public String PhanTich(long a){
+        String s ="";
+        for(int i=2;i<=a;i++){
+            while(a%i==0){
+                s+= i + " * ";
+                a/=i;
+            }
+        }
+        s=s.substring(0,s.length()-3);
+        return s;
+    }
 }
--- src/test/java/PTThuaSoNguyenToTest.java
+++ src/test/java/PTThuaSoNguyenToTest.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PTThuaSoNguyenToTest {
+    @Test
+    public void Test_PhanTich(){
+        Assert.assertEquals(new PTThuaSoNguyenTo().PhanTich(600), "2 * 2 * 2 * 3 * 5 * 5");
+    }
 }
