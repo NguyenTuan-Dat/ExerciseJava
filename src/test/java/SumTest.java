3b49bab7a6535d0df63206ca6e22ba16a03324df
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-15 12:53:52 +0700

    Init

---
+++ src/test/java/SumTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SumTest {
+    @Test
+    public void Test_SUM()
+    {
+        Assert.assertEquals(new Sum().SUM(123),6);
+    }
+}
