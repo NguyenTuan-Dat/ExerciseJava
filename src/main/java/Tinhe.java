f295299e2259727d51219672474158afb9d343fb
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-16 12:01:31 +0700

    Update

--- src/main/java/Tinhe.java
+++ src/main/java/Tinhe.java
@@ -4,7 +4,7 @@ public class Tinhe {
         long giaithua = 1;
         long i=2;
         double E = 2.718281828;
-        while(e-E>0.000001 || E-e>0.000001){
+        while(Math.abs(E-e)>0.000001){
             giaithua*=i;
             i++;
             e+=1.0/giaithua;
