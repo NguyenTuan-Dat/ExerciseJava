9a9b60c221b5b61f261ae727f653a8fdfded5d1f
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-16 10:20:17 +0700

    Init

---
+++ src/test/java/TinhPiBai10Test.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TinhPiBai10Test {
+    @Test
+    public void test_tinhpi(){
+        Assert.assertEquals(new TinhPiBai10().tinhpi(),"3.14159");
+    }
+}
