bc5b6542e6feaf12d9531a4591931a77c1bfb839
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-18 20:42:31 +0700

    Update

--- src/main/java/TinhS.java
+++ src/main/java/TinhS.java
@@ -1,11 +1,13 @@
 public class TinhS {
-    public String S(long n){
-        double s=2;
-        long giaithua=1;
-        for(int i=2; i<=n; i++){
-            giaithua*=i;
-            s+=1.0/giaithua;
+    public double S(long n) {
+        double s = 2;
+        long giaithua = 1;
+
+        for (int i = 2; i <= n; i++) {
+            giaithua *= i;
+            s += 1.0 / giaithua;
         }
-        return String.valueOf(s);
+
+        return s;
     }
 }
--- src/test/java/TinhSTest.java
+++ src/test/java/TinhSTest.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 public class TinhSTest {
     @Test
     public void test_tinhs(){
-        Assert.assertEquals(new TinhS().S(3),"2.6666666666666665");
+        Assert.assertEquals(new TinhS().S(3),2.66666666666667, 0.0000001);
     }
 }
