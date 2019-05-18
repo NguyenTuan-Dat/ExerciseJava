0b193822f2617376c0a9596e508b43414dab703e
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-18 20:16:50 +0700

    Update

--- src/test/java/PTBac2Test.java
+++ src/test/java/PTBac2Test.java
@@ -5,7 +5,7 @@ public class PTBac2Test {
     @Test
     public void test_giaiphuongtrinh(){
         Assert.assertEquals(new PTBac2().giaiphuongtrinh(1,2,3), "Phương trình vô nghiệm");
-        Assert.assertEquals(new PTBac2().giaiphuongtrinh(1,4,4), "x1= -2.0, x2= -2.0");
+        Assert.assertEquals(new PTBac2().giaiphuongtrinh(1,4,4), "x1 = x2 = -2.0");
         Assert.assertEquals(new PTBac2().giaiphuongtrinh(1,4,3), "x1= -1.0, x2= -3.0");
     }
 }
