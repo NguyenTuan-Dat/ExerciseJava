3f1171b2f27da7cbf69aca425fa5abddc5ddd679
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-18 20:26:22 +0700

    add-exercise-10

--- src/main/java/TinhPiBai10.java
+++ src/main/java/TinhPiBai10.java
@@ -1,15 +1,14 @@
 public class TinhPiBai10 {
-    public String tinhpi(){
+    public double tinhpi(){
         double pi=3;
         long i=2,a=1;
-        while(pi-3.141592654>0.000001 || 3.141592654-pi>0.000001){
+
+        while(Math.abs(pi-3.141592654)>0.000001){
             long mau=i*(i+1)*(i+2);
             pi+=(double)a*4/mau;
             a*=-1;
             i+=2;
         }
-        String s=String.valueOf(pi);
-        s=s.substring(0,7);
-        return s;
+        return pi;
     }
 }
