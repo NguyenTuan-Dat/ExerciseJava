a04a89542400ff73023b69af90a7e8fe0f6f74dc
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-18 20:48:09 +0700

    Update TinhCanTest.java

--- src/test/java/TinhCanTest.java
+++ src/test/java/TinhCanTest.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 public class TinhCanTest {
     @Test
     public void test_can(){
-        Assert.assertEquals(new TinhCan().can(8), Math.sqrt(8), 0.00001);
+        Assert.assertEquals(new TinhCan().sqrt(8), Math.sqrt(8), 0.00001);
     }
 }
