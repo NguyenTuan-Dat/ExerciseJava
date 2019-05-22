113d9713da2fc95bff03fd99b397db2020dbe419
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-22 17:04:26 +0700

    Update

--- src/main/java/SoNguyenThoaMan.java
+++ src/main/java/SoNguyenThoaMan.java
@@ -1,11 +1,15 @@
 public class SoNguyenThoaMan {
+    int tinhdem(int a, int dem){
+        if (a % Math.pow(10, dem) == 0) dem++;
+        return dem;
+    }
     public String find() {
         String s = "";
         int dem = 2;
         for (int i = 10; i < 100000; i++) {
             int a = i;
             int kq = 0;
-            if (a % Math.pow(10, dem) == 0) dem++;
+            dem = tinhdem(a,dem);
             while (a > 0) {
                 int x = a % 10;
                 a/=10;
@@ -13,6 +17,6 @@ public class SoNguyenThoaMan {
             }
             if (kq == i) s += i + " ";
         }
-        return s.substring(0,s.length()-1);
+        return s.trim();
     }
 }
