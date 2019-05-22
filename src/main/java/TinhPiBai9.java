dc3ad0b1c64b9a41ebc8549eb9e2f912b00895d5
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-22 17:39:11 +0700

    add-exercise-9

--- src/main/java/TinhPiBai9.java
+++ src/main/java/TinhPiBai9.java
@@ -3,11 +3,13 @@ public class TinhPiBai9 {
         double pi=0;
         int a=1;
         long i=1;
-        while(pi-3.141592654>0.000001 || 3.141592654-pi>0.000001){
+        double PI;
+        do {
+            PI=pi;
             pi += (double)a * 4 / i;
             a *= -1;
             i+=2;
-        }
+        }while(Math.abs(PI-pi)>0.000001);
         return pi;
     }
 }
