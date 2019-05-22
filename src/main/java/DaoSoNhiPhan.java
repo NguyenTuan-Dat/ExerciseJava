4b7e3c343ba43e096dce02e69c6f8d06be020b94
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-22 16:54:05 +0700

    Update

--- src/main/java/DaoSoNhiPhan.java
+++ src/main/java/DaoSoNhiPhan.java
@@ -1,17 +1,16 @@
 public class DaoSoNhiPhan {
     public long tinh(long n){
-        String s="";
+        long a=0;
         while(n>0){
-            if(n%2==0) s+='0';
-            else s+='1';
+            a=a*10+n%2;
             n/=2;
         }
         long kq=0;
         long dem=0;
         do {
-            kq+=(s.charAt(s.length()-1)-'0')*Math.pow(2,dem++);
-            s=s.substring(0,s.length()-1);
-        }while(s.length()>0);
+            kq+=a%10*Math.pow(2,dem++);
+            a/=10;
+        }while(a>0);
         return kq;
     }
 }
