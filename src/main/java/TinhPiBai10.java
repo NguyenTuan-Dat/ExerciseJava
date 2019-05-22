bf5cf75949c5dcf3ef7445831996c72e17254583
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-22 17:35:34 +0700

    Update

--- src/main/java/TinhPiBai10.java
+++ src/main/java/TinhPiBai10.java
@@ -2,13 +2,14 @@ public class TinhPiBai10 {
     public double tinhpi(){
         double pi=3;
         long i=2,a=1;
-
-        while(Math.abs(pi-3.141592654)>0.000001){
+        double PI;
+        do {
             long mau=i*(i+1)*(i+2);
+            PI=pi;
             pi+=(double)a*4/mau;
             a*=-1;
             i+=2;
-        }
+        } while(Math.abs(pi-PI)>0.000001);
         return pi;
     }
 }
