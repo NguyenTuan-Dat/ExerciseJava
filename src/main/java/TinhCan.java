77df2aad52f3479e94c6047182a31436ad206b59
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-22 17:27:15 +0700

    Update

--- src/main/java/TinhCan.java
+++ src/main/java/TinhCan.java
@@ -1,9 +1,11 @@
 public class TinhCan {
     public double sqrt(double a) {
         double f = a;
-        for(int i=0; i<1000; i++){
+        double f1;
+        do {
+            f1=f;
             f = a / 2 / f + f / 2;
-        }
+        }while(f1-f>0.00001);
         return f;
     }
 }
--- src/test/java/TinhCanTest.java
+++ src/test/java/TinhCanTest.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 public class TinhCanTest {
     @Test
     public void test_sqrt(){
-        Assert.assertEquals(new TinhCan().sqrt(8.0), Math.sqrt(8), 0.00001);
+        Assert.assertEquals(new TinhCan().sqrt(8.0), 2.828427125, 0.00001);
     }
 }
