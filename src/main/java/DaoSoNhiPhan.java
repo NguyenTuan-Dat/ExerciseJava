9d731ef3b17017076abc2d96a0ded5146b418b30
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-23 10:04:41 +0700

    Update DaoSoNhiPhan.java

--- src/main/java/DaoSoNhiPhan.java
+++ src/main/java/DaoSoNhiPhan.java
@@ -1,16 +1,20 @@
 public class DaoSoNhiPhan {
     public long tinh(long n){
         long a=0;
+        
         while(n>0){
             a=a*10+n%2;
             n/=2;
         }
+        
         long kq=0;
         long dem=0;
+        
         do {
             kq+=a%10*Math.pow(2,dem++);
             a/=10;
         }while(a>0);
+        
         return kq;
     }
 }
