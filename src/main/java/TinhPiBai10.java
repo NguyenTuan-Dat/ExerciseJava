2fc7e386430abe209b076153334d82271ca34d27
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-23 12:52:45 +0700

    Update TinhPiBai10.java

--- src/main/java/TinhPiBai10.java
+++ src/main/java/TinhPiBai10.java
@@ -1,15 +1,17 @@
 public class TinhPiBai10 {
+    
     public double pi(){
-        double pi=3,p;
-        long i=2,a=1;
+        
+        double pi= 3 , p;
+        long i = 2 , a = 1;
 
         do {
-            long mau=i*(i+1)*(i+2);
+            long mau = i * ( i + 1 ) * ( i + 2 );
             p=pi;
-            pi+=(double)a*4/mau;
-            a*=-1;
-            i+=2;
-        } while(Math.abs(pi-p)>0.000001);
+            pi+=(double) a * 4 / mau;
+            a *= -1;
+            i += 2;
+        } while(Math.abs( pi - p ) > 0.000001);
 
         return pi;
     }
