50003840176e1a04ea6fc6cc3931ac6d5a53f1cb
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-23 09:48:07 +0700

    Update

--- src/main/java/TinhPiBai10.java
+++ src/main/java/TinhPiBai10.java
@@ -1,15 +1,16 @@
 public class TinhPiBai10 {
-    public double tinhpi(){
-        double pi=3;
+    public double pi(){
+        double pi=3,p;
         long i=2,a=1;
-        double PI;
+
         do {
             long mau=i*(i+1)*(i+2);
-            PI=pi;
+            p=pi;
             pi+=(double)a*4/mau;
             a*=-1;
             i+=2;
-        } while(Math.abs(pi-PI)>0.000001);
+        } while(Math.abs(pi-p)>0.000001);
+
         return pi;
     }
 }
--- src/test/java/TinhPiBai10Test.java
+++ src/test/java/TinhPiBai10Test.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 public class TinhPiBai10Test {
     @Test
     public void test_tinhpi(){
-        Assert.assertEquals(new TinhPiBai10().tinhpi(),3.141592654,0.00001);
+        Assert.assertEquals(new TinhPiBai10().pi(),3.141592654,0.00001);
     }
 }
