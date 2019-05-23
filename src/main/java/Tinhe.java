9e2511948c7bd626c75163e69ddbb64021ccf2d3
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-23 09:53:37 +0700

    Update Tinhe.java

--- src/main/java/Tinhe.java
+++ src/main/java/Tinhe.java
@@ -1,16 +1,16 @@
 public class Tinhe {
     public double tinhe(){
-        double e=2;
+        double e = 2;
         long giaithua = 1;
-        long i=2;
-        double E;
+        long i = 2;
+        double euler;
         do {
-            giaithua*=i;
+            giaithua *= i;
             i++;
-            E=e;
+            euler = e;
             e += 1.0 / giaithua;
-        }while(e-E > 0.000001);
-        //return String.valueOf(e).substring(0,8);
+        }while(e - euler > 0.000001);
+        
         return e;
     }
 }
