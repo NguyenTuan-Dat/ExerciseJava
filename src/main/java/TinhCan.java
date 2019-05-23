de37b7b21c7f048f3d6747dcb6b42c6f2797fc46
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-23 09:50:54 +0700

    Update TinhCan.java

--- src/main/java/TinhCan.java
+++ src/main/java/TinhCan.java
@@ -2,10 +2,12 @@ public class TinhCan {
     public double sqrt(double a) {
         double f = a;
         double f1;
+        
         do {
             f1=f;
             f = a / 2 / f + f / 2;
         }while(f1-f>0.00001);
+        
         return f;
     }
 }
