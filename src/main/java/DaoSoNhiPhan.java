6885b5504599a6cf3edda86f1bd12b4f9fdf1322
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-23 10:06:07 +0700

    Update DaoSoNhiPhan.java

--- src/main/java/DaoSoNhiPhan.java
+++ src/main/java/DaoSoNhiPhan.java
@@ -3,17 +3,17 @@ public class DaoSoNhiPhan {
         long a=0;
         
         while(n>0){
-            a=a*10+n%2;
-            n/=2;
+            a = a * 10 + n % 2;
+            n /= 2;
         }
         
         long kq=0;
         long dem=0;
         
         do {
-            kq+=a%10*Math.pow(2,dem++);
-            a/=10;
-        }while(a>0);
+            kq += a % 10 * Math.pow(2,dem++);
+            a /= 10;
+        }while(a > 0);
         
         return kq;
     }
