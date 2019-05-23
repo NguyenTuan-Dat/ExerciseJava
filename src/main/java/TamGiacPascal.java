efd493cb2acad7665d715106701d2b3d3c21555d
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-23 10:19:41 +0700

    Update

--- src/main/java/TamGiacPascal.java
+++ src/main/java/TamGiacPascal.java
@@ -1,20 +1,36 @@
 public class TamGiacPascal {
+
+    public int[] xuongdong(int a[]) {
+        int b[] = new int[a.length + 1];
+        b[0] = 1;
+        b[b.length - 1] = 1;
+
+        for (int i = 1; i < b.length - 1; i++) {
+            b[i] = a[i - 1] + a[i];
+        }
+
+        return b;
+    }
+
+    public String string(int a[]) {
+        String s = "";
+
+        for (int i = 0; i < a.length; i++) {
+            s += a[i] + " ";
+        }
+
+        return s.trim();
+    }
+
     public String tinh(int n) {
         String s = "";
-        int A[] = {1};
+        int a[] = {1};
         int dem = 1;
+
         while (dem++ < n) {
-            int B[] = new int[A.length + 1];
-            B[0] = 1;
-            B[B.length-1] = 1;
-            for (int i = 1; i < B.length - 1; i++) {
-                B[i] = A[i-1] + A[i];
-            }
-            A = B;
+            a = xuongdong(a);
         }
-        for (int i = 0; i < A.length; i++) {
-            s += A[i] + " ";
-        }
-        return s.trim();
+
+        return string(a);
     }
 }
