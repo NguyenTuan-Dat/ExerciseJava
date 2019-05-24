1e90474a4dd07be0cdf1f7084932a777b38c14e4
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-24 09:27:46 +0700

    Update

--- src/main/java/KiemTraTamGiacBai16.java
+++ src/main/java/KiemTraTamGiacBai16.java
@@ -2,32 +2,47 @@ import java.util.Arrays;
 
 public class KiemTraTamGiacBai16 {
 
-    double[] dodai(double X[], double Y[]) {
-        double A[] = new double[3];
-        A[0] = Math.sqrt(Math.pow((X[0] - X[1]), 2) + Math.pow((Y[0] - Y[1]), 2));
-        A[1] = Math.sqrt(Math.pow((X[1] - X[2]), 2) + Math.pow((Y[1] - Y[2]), 2));
-        A[2] = Math.sqrt(Math.pow((X[2] - X[0]), 2) + Math.pow((Y[2] - Y[0]), 2));
-        return A;
+    double dodai(double x1, double x2, double y1, double y2) {
+
+        double x = x1 - x2;
+        double y = y1 - y2;
+
+        return Math.sqrt(x*x+y*y);
     }
 
-    public String kiemtra(double X[], double Y[]) {
-        double A[] = dodai(X, Y);
-        Arrays.sort(A);
-        if (A[0] + A[1] <= A[2]) {
+    public String kiemtra(double x[], double y[]) {
+
+        double a[] = new double[3];
+        a[0] = dodai(x[0],x[1],y[0],y[1]);
+        a[1] = dodai(x[1],x[2],y[1],y[2]);
+        a[2] = dodai(x[2],x[0],y[2],y[0]);
+        Arrays.sort(a);
+
+        if (a[0] + a[1] <= a[2]) {
+
             return new String("Tam giác không hợp lệ");
+
         } else {
-            String result = "";
-            double C = A[0] + A[1] + A[2];
+
+            double C = a[0] + a[1] + a[2];
             double p = C / 2;
-            double area = Math.sqrt(p * (p - A[0]) * (p - A[1]) * (p - A[2]));
-            result = "S=" + area + ", C=" + C + ", tam giác là tam giác";
-            if (A[2] * A[2] == A[0] * A[0] + A[1] * A[1]) {
+            double area = Math.sqrt(p * (p - a[0]) * (p - a[1]) * (p - a[2]));
+
+            String result = "S=" + area + ", C=" + C + ", tam giác là tam giác";
+            if (a[2] * a[2] == a[0] * a[0] + a[1] * a[1]) {
+
                 result += " vuông";
+
             } else {
-                if (A[0] == A[1] || A[1] == A[2]) {
+
+                if (a[0] == a[1] || a[1] == a[2]) {
+
                     result += " cân";
+
                 } else {
+
                     result += " bình thường";
+
                 }
             }
             return result;
