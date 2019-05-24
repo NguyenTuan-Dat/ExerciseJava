a63e32a52bbd31300bbf5fb0bbc416316d25ddda
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-24 09:17:32 +0700

    Update SoNguyenThoaMan.java

--- src/main/java/SoNguyenThoaMan.java
+++ src/main/java/SoNguyenThoaMan.java
@@ -1,22 +1,39 @@
 public class SoNguyenThoaMan {
-    int tinhdem(int a, int dem){
+    
+    int tinhDem(int a, int dem){
+    
         if (a % Math.pow(10, dem) == 0) dem++;
+       
         return dem;
     }
-    public String find() {
-        String s = "";
-        int dem = 2;
-        for (int i = 10; i < 100000; i++) {
-            int a = i;
-            int kq = 0;
-            dem = tinhdem(a,dem);
-            while (a > 0) {
+    
+    int tinhKq(int a , int dem){
+    
+        int kq=0;
+        while (a > 0) {
                 int x = a % 10;
                 a/=10;
                 kq += Math.pow(x, dem);
             }
+        
+        return kq;
+    }
+    
+    public String find() {
+        
+        String s = "";
+        int dem = 2;
+        
+        for (int i = 10; i < 100000; i++) {
+            
+            int a = i;
+            dem = tinhDem(a,dem);
+            int kq = tinhKq(a,dem);
             if (kq == i) s += i + " ";
+        
         }
+        
         return s.trim();
+    
     }
 }
