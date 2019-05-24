29445c61d2341b13d5b06ebeffaf4d10f968ac43
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-24 10:30:07 +0700

    Update DichSoLaMaSangHe10Test.java

--- src/test/java/DichSoLaMaSangHe10Test.java
+++ src/test/java/DichSoLaMaSangHe10Test.java
@@ -7,6 +7,7 @@ public class DichSoLaMaSangHe10Test {
     public void test_dich(){
 
         Assert.assertEquals(new DichSoLaMaSangHe10().dich("CCXXIX"), 229);
+        Assert.assertEquals(new DichSoLaMaSangHe10().dich("CCXXIX"), 224);
         Assert.assertEquals(new DichSoLaMaSangHe10().dich("CCLXXXIX"), 289);
         Assert.assertEquals(new DichSoLaMaSangHe10().dich("MMMCMXCIX"),3999);
         Assert.assertEquals(new DichSoLaMaSangHe10().dich("MMMDCCCLXXXVIII"),3888);
