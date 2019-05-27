ac754fa9817e0462b3e822f8af08c8db4bbe5faf
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-27 09:00:32 +0700

    Update

--- src/main/java/SoNguyenThoaMan.java
+++ src/main/java/SoNguyenThoaMan.java
@@ -1,39 +1,36 @@
 public class SoNguyenThoaMan {
-    
-    int tinhDem(int a, int dem){
-    
+
+    int timSoMu(int a, int dem) {
+
         if (a % Math.pow(10, dem) == 0) dem++;
-       
+
         return dem;
     }
-    
-    int tinhKq(int a , int dem){
-    
-        int kq=0;
+
+    int tinhKq(int a, int dem) {
+
+        int kq = 0;
         while (a > 0) {
-                int x = a % 10;
-                a/=10;
-                kq += Math.pow(x, dem);
-            }
-        
+            int x = a % 10;
+            a /= 10;
+            kq += Math.pow(x, dem);
+        }
+
         return kq;
     }
-    
+
     public String find() {
-        
+
         String s = "";
         int dem = 2;
-        
+
         for (int i = 10; i < 100000; i++) {
-            
-            int a = i;
-            dem = tinhDem(a,dem);
-            int kq = tinhKq(a,dem);
+            dem = timSoMu(i, dem);
+            int kq = tinhKq(i, dem);
             if (kq == i) s += i + " ";
-        
+
         }
-        
+
         return s.trim();
-    
     }
 }
