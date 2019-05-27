ca8e1d645633138d2665b04b18d5ecd5ea054ef3
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-27 08:57:22 +0700

    Update

--- src/main/java/KiemTraTamGiacBai16.java
+++ src/main/java/KiemTraTamGiacBai16.java
@@ -7,15 +7,15 @@ public class KiemTraTamGiacBai16 {
         double dx = x1 - x2;
         double dy = y1 - y2;
 
-        return Math.sqrt(x*x+y*y);
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     public String kiemtra(double x[], double y[]) {
 
         double a[] = new double[3];
-        a[0] = dodai(x[0],x[1],y[0],y[1]);
-        a[1] = dodai(x[1],x[2],y[1],y[2]);
-        a[2] = dodai(x[2],x[0],y[2],y[0]);
+        a[0] = dodai(x[0], x[1], y[0], y[1]);
+        a[1] = dodai(x[1], x[2], y[1], y[2]);
+        a[2] = dodai(x[2], x[0], y[2], y[0]);
         Arrays.sort(a);
 
         if (a[0] + a[1] <= a[2]) {
