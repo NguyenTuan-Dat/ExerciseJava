d80e29d9aac2e5ace5f766fb8c00dc4fed1538d6
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-28 19:21:24 +0700

    Update Array2.java

--- src/main/java/Array2.java
+++ src/main/java/Array2.java
@@ -4,7 +4,7 @@ public class Array2 {
 
     public int sum(int a[]) {
 
-        Arrays.sort(a, Colections.reverseOrder());
+        Arrays.sort(a, Collections.reverseOrder());
 
         return a[0]+a[1]+a[2];
     }
