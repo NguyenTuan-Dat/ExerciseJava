63c49add60b4dc527b8cff27520d76d36faf6572
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-29 15:06:03 +0700

    Init

---
+++ src/test/java/Array19Test.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Array19Test {
+    @Test
+    public void test_sxMaTranXoanOc() {
+        int a[][] = {{1, 5, 4, 3, 2}, {6, 7, 8, 9, 10}, {12, 13, 11, 14, 15}, {16, 17, 18, 19, 20}};
+        int b[][] = {{1, 2, 3, 4, 5}, {14, 15, 16, 17, 6}, {13, 20, 19, 18, 7}, {12, 11, 10, 9, 8}};
+        Assert.assertArrayEquals(new Array19().sxMaTranXoanOc(a), b);
+    }
+}
