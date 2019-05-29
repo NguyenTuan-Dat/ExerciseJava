f787321a1a061cdb458d5ac0428c71c8df6ca369
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-05-29 13:07:13 +0700

    Update Array14Test.java

--- src/test/java/Array14Test.java
+++ src/test/java/Array14Test.java
@@ -7,5 +7,8 @@ public class Array14Test {
         int[][] a = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
         int[] b = {1, 2, 3, 4, 5, 10, 15, 14, 13, 12, 11, 6, 7, 8, 9};
         Assert.assertArrayEquals(new Array14().chuyenDoiMang(a), b);
+        int[][] c= {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15},{16,17,18,19,20}};
+        int [] d = {1,2,3,4,5,10,15,20,19,18,17,16,11,6,7,8,9,14,13,12};
+        Assert.assertArrayEquals(new Array14().chuyenDoiMang(c), d);
     }
 }
