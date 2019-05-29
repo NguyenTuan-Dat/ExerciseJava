5f81a53005b8b80a669c7c987ac606ab6100e12c
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-29 09:56:31 +0700

    Update

--- src/main/java/Array7.java
+++ src/main/java/Array7.java
@@ -1,33 +1,28 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Array7 {
 
     public int[] findArray(int a[]) {
 
-        List<Integer> max = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
+        int b[] = new int[a.length];
+        b[b.length-1] = 1;
 
-        for (int i = 0; i < a.length - 1; i++) {
-            if (a[i] < a[i + 1]) b.add(a[i]);
-            else {
-                b.add(a[i]);
-                if (max.size() < b.size() + 1) {
-                    max.clear();
-                    max.addAll(b);
-                    b.clear();
-                } else b.clear();
-            }
-            if (i == a.length - 2) {
-                b.add(a[i + 1]);
-                if (max.size() < b.size() + 1) {
-                    max.clear();
-                    max.addAll(b);
-                    b.clear();
-                }
+        for (int i = b.length-2; i >=0 ; i--) {
+            if (a[i+1] >= a[i]) b[i] = b[i+1] + 1;
+            else b[i] = 1;
+        }
+
+        int max=0, local=0;
+        for (int i = 0; i < b.length; i++) {
+            if(b[i]>max)
+            {
+                max=b[i];
+                local=i;
             }
         }
 
-        return max.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.copyOfRange(a,local, local+max);
     }
 }
--- src/test/java/Array7Test.java
+++ src/test/java/Array7Test.java
@@ -7,8 +7,8 @@ public class Array7Test {
         int a[] = {1, 2, 3, 4, 1};
         int b[] = {1, 2, 3, 4};
         Assert.assertArrayEquals(new Array7().findArray(a), b);
-        int c[] = {1, 2, 3, 4, 1, 5, 6, 7, 8, 9, 10};
-        int d[] = {1, 5, 6, 7, 8, 9, 10};
+        int c[] = {1, 2, 3, 4, 1, 5, 6, 7, 8, 9, 10, 11};
+        int d[] = {1, 5, 6, 7, 8, 9, 10, 11};
         Assert.assertArrayEquals(new Array7().findArray(c), d);
         int e[] = {1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 3, 4, 5, 6, 9};
         int f[] = {1, 2, 3, 4, 5, 6, 9};
