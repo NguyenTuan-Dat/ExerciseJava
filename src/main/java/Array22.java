60a31125c1c1a58da8a095c60c9573157956ddf4
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-29 17:20:29 +0700

    Update

--- src/main/java/Array22.java
+++ src/main/java/Array22.java
@@ -6,12 +6,10 @@ public class Array22 {
 
         for (int i = 0; i < b.length; i++) {
             for (int j = 0; j < b[0].length; j++) {
-                if (i >= x && j >= y) b[i][j] = a[i + 1][j + 1];
-                else if (i >= x && j < y) b[i][j] = a[i + 1][j];
-                else if (i < x && j >= y) b[i][j] = a[i][j + 1];
-                else {
-                    b[i][j] = a[i][j];
-                }
+                int k = 0, l = 0;
+                if (i >= x) k = 1;
+                if (j >= y) l = 1;
+                b[i][j] = a[i + k][j + l];
             }
         }
 
