e67712268b4ca92320424d211f2870ae2e806eb3
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-29 16:51:56 +0700

    Update

--- src/test/java/Array23Test.java
+++ src/test/java/Array23Test.java
@@ -3,8 +3,12 @@ import org.junit.Test;
 
 public class Array23Test {
     @Test
-    public void test_tongDaySoTuNhien(){
+    public void test_tongDaySoTuNhien() {
         int a[] = {0, 1, -2, 8, 9, 0, 1, -2, 1, 1, 3, 1, 1};
-        Assert.assertEquals(new Array23().tongDaySoTuNhien(a),18);
+        int b[] = {-1, 1, -2, 8, 9, 0, 1, -2, 1, 1, 3, 1, 1};
+        int c[] = {1, 1, -2, 8, 9, 0, 1, -2, 1, 1, 3, 1, 1};
+        Assert.assertEquals(new Array23().tongDaySoTuNhien(a), 18);
+        Assert.assertEquals(new Array23().tongDaySoTuNhien(b), 18);
+        Assert.assertEquals(new Array23().tongDaySoTuNhien(c), 18);
     }
 }
