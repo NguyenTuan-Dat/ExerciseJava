5c83c0df4acabeccda6e11850c49c222a4c28caf
bkdnntdat <bkdn.ntdat@gmail.com>
2019-05-29 16:37:34 +0700

    Update

--- src/test/java/Array22Test.java
+++ src/test/java/Array22Test.java
@@ -8,8 +8,12 @@ public class Array22Test {
         int b[][] = {{5, 6}, {8, 9}, {11, 12}};
         int c[][] = {{1, 2}, {7, 8}, {10, 11}};
         int d[][] = {{1, 2}, {4, 5}, {10, 11}};
+        int e[][] = {{2, 3}, {5, 6}, {11, 12}};
+        int f[][] = {{4, 5}, {7, 8}, {10, 11}};
         Assert.assertArrayEquals(new Array22().xoaHangXoaCot(a, 0, 0), b);
         Assert.assertArrayEquals(new Array22().xoaHangXoaCot(a, 1, 2), c);
         Assert.assertArrayEquals(new Array22().xoaHangXoaCot(a, 2, 2), d);
+        Assert.assertArrayEquals(new Array22().xoaHangXoaCot(a, 2, 0), e);
+        Assert.assertArrayEquals(new Array22().xoaHangXoaCot(a, 0, 2), f);
     }
 }
