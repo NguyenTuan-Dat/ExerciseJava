ef1436ca292791d3d7210d342ff8c394cbf80354
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-04 09:23:03 +0700

    Update

--- src/main/java/Array11.java
+++ src/main/java/Array11.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Array11 {
-    public static boolean swapped[];
-    public static int b[];
+    public static boolean swapped[] = {false, false, false};
+    public static int b[] = {1,2,3};
 
-    public static void swap(boolean even, int x) {
+    public int[] swap(boolean even, int x) {
         for (int i = b.length - 1; i >= 0; i--)
             if ((!swapped[i]) && (((b[i] % 2) == 0) == even)) {
                 swapped[i] = true;
@@ -16,21 +16,18 @@ public class Array11 {
                 b[i] = c;
                 break;
             }
-
+        return b;
     }
 
-    public static int[] swapEvenAndOdd(int a[]) {
+    public int[] swapEvenAndOdd(int a[]) {
 
         b = a;
         swapped = new boolean[a.length];
-        for(int i=0; i<a.length; i++) swapped[i]=false;
+        for (int i = 0; i < a.length; i++) swapped[i] = false;
 
         for (int i = 0; i < a.length; i++) {
-            boolean even;
-            if (b[i] % 2 == 0 && !swapped[i]) {
-                even = true;
-            } else even = false;
-            swap(even,i);
+            boolean even = b[i] % 2 == 0 && !swapped[i];
+            swap(even, i);
         }
 
         return b;
