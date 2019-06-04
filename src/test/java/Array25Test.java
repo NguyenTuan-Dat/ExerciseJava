1172970ccda58ca3d60ce98a8fa68435366ed516
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-04 09:33:27 +0700

    Update

--- src/test/java/Array25Test.java
+++ src/test/java/Array25Test.java
@@ -7,8 +7,12 @@ public class Array25Test {
         int a[] = {1,2,3};
         int b[] = {1,2,3,4};
         int c[] = {2,6,8};
+        int d[] = {0,1,2,3,4};
+        int e[] = {8,6,2};
         Assert.assertEquals(new Array25().uclnOfArray(a),1);
         Assert.assertEquals(new Array25().uclnOfArray(b),1);
         Assert.assertEquals(new Array25().uclnOfArray(c),2);
+        Assert.assertEquals(new Array25().uclnOfArray(d),1);
+        Assert.assertEquals(new Array25().uclnOfArray(e),2);
     }
 }
