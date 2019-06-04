21a5982dda7b523515bd38eb725aa98ba9d33310
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-06-04 09:47:39 +0700

    Update Array11Test.java

--- src/test/java/Array11Test.java
+++ src/test/java/Array11Test.java
@@ -2,15 +2,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Array11Test {
+    
+    @Test
+    public void test_swap() {
+        int c[] = {3,2,1};
+        Assert.assertArrayEquals(new Array11().swap(false,0),c);
+    
     @Test
     public void test_swapEvenAndOdd() {
         int a[] = {1, 2, 4, 5, 6, 7, 8, 10};
         int b[] = {7, 10, 8, 5, 6, 1, 4, 2};
         Assert.assertArrayEquals(new Array11().swapEvenAndOdd(a),b);
     }
-    
-    @Test
-    public void test_swap() {
-        int c[] = {3,2,1};
-        Assert.assertArrayEquals(new Array11().swap(false,0),c);
 }
