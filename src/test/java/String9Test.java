979508ea7bed1392718ed73efb35b2b1b2ed61a9
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-06-07 16:40:11 +0700

    Update String9Test.java

--- src/test/java/String9Test.java
+++ src/test/java/String9Test.java
@@ -6,8 +6,8 @@ public class String9Test {
     public void test_rutGon(){
         String a = "abcccceeeeeefd";
         String b = "abc4e6fd";
-        String c = "abbbbbbbbbbbbbc";
-        String d = "ab13c";
+        String c = "abbbbbbbbbbbbbcc";
+        String d = "ab13c2";
         Assert.assertEquals(new String9().rutGon(a),b);
         Assert.assertEquals(new String9().rutGon(c),d);
     }
