c57aea4d6573a4c6b5985e7fc2d02f366ed25a35
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-07 13:52:39 +0700

    Update

--- src/test/java/String4Test.java
+++ src/test/java/String4Test.java
@@ -5,7 +5,7 @@ public class String4Test {
     @Test
     public void test_sum(){
         Assert.assertEquals(new String4().sum("12 asd2.5"), "19");
-        Assert.assertEquals(new String4().sum("12 asd2.6"), "20");
+        Assert.assertEquals(new String4().sum("12 asd2.6aa"), "20");
         Assert.assertEquals(new String4().sum("82 asd2.7"), "91");
         Assert.assertEquals(new String4().sum("2 asd32.5"), "39");
     }
@@ -15,5 +15,6 @@ public class String4Test {
         Assert.assertEquals(new String4().tinhTong("1224","2337"),"3561");
         Assert.assertEquals(new String4().tinhTong("12","2"),"14");
         Assert.assertEquals(new String4().tinhTong("14","5"),"19");
+        Assert.assertEquals(new String4().tinhTong("14","7"),"21");
     }
 }
