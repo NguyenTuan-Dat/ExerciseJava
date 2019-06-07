7bf1424df108180ebc2d91a44a03dd3b118704f4
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-06-07 15:29:36 +0700

    Update String7Test.java

--- src/test/java/String7Test.java
+++ src/test/java/String7Test.java
@@ -6,6 +6,8 @@ public class String7Test {
     public void test_findLocal(){
         String a = "aaa sdshdasdaaa";
         String b = "asd";
+        String c = "bbbb";
         Assert.assertEquals(new String7().findLocal(a,b),9);
+        Assert.assertEquals(new String7().findLocal(a,c),-1);
     }
 }
