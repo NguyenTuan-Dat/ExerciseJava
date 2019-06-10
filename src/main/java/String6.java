40d4c3c2a3dc45884d532b77ded6da624674418c
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-10 18:19:13 +0700

    Update

--- src/main/java/String6.java
+++ src/main/java/String6.java
@@ -2,17 +2,14 @@ public class String6 {
     public String xuLyChuoi(String a) {
         String c = "";
         for (int i = 0; i < a.length(); i++) {
+
             if (Character.isUpperCase(a.charAt(i))) {
                 c += Character.toLowerCase(a.charAt(i));
             } else {
-                if (Character.isLowerCase(a.charAt(i))) {
-                    c += Character.toUpperCase(a.charAt(i));
-                }
-                else{
-                    c+= a.charAt(i);
-                }
+                c += Character.toUpperCase(a.charAt(i));
             }
         }
+
         return c;
     }
 }
