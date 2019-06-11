c60f9dc899f2d95500d75b3437e3258544593b63
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-11 16:24:13 +0700

    Up

--- src/test/java/String19Test.java
+++ src/test/java/String19Test.java
@@ -15,13 +15,13 @@ public class String19Test {
     }
 
     @Test
-    public void test_tinhHieu(){
-        Assert.assertEquals(new String19().tinhHieu("351","12"),"339");
-        Assert.assertEquals(new String19().tinhHieu("326","199"),"127");
-        Assert.assertEquals(new String19().tinhHieu("199","326"),"-127");
-        Assert.assertEquals(new String19().tinhHieu("199","199"),"0");
-        Assert.assertEquals(new String19().tinhHieu("12","351"),"-339");
-        Assert.assertEquals(new String19().tinhHieu("17","9"),"8");
+    public void test_tinhHieu() {
+        Assert.assertEquals(new String19().tinhHieu("351", "12"), "339");
+        Assert.assertEquals(new String19().tinhHieu("326", "199"), "127");
+        Assert.assertEquals(new String19().tinhHieu("199", "326"), "-127");
+        Assert.assertEquals(new String19().tinhHieu("199", "199"), "0");
+        Assert.assertEquals(new String19().tinhHieu("12", "351"), "-339");
+        Assert.assertEquals(new String19().tinhHieu("17", "9"), "8");
     }
 
     @Test
