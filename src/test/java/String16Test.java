6b4f676c1cfd274f45e65b6d6ef32963400f08d1
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-11 13:33:27 +0700

    Update

--- src/test/java/String16Test.java
+++ src/test/java/String16Test.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 public class String16Test {
     @Test
     public void test_tinhTong(){
-        Assert.assertEquals(new String4().tinhTong("1224","2337"),"3561");
-        Assert.assertEquals(new String4().tinhTong("12","2"),"14");
-        Assert.assertEquals(new String4().tinhTong("14","5"),"19");
-        Assert.assertEquals(new String4().tinhTong("14","7"),"21");
-        Assert.assertEquals(new String4().tinhTong("14","87"),"101");
+        Assert.assertEquals(new String16().tinhTong("1224","2337"),"3561");
+        Assert.assertEquals(new String16().tinhTong("12","2"),"14");
+        Assert.assertEquals(new String16().tinhTong("14","5"),"19");
+        Assert.assertEquals(new String16().tinhTong("14","7"),"21");
+        Assert.assertEquals(new String16().tinhTong("14","87"),"101");
     }
 }
