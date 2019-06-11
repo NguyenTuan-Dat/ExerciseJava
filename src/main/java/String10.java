03cff032a8b5d66ae28a57788c838fdf593228b0
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-11 09:16:51 +0700

    Update

--- src/main/java/String10.java
+++ src/main/java/String10.java
@@ -1,26 +1,27 @@
 public class String10 {
     public String khaiTrien(String a) {
         a += " ";
-        String c = "";
+        StringBuilder c = new StringBuilder("");
 
         for (int i = 0; i < a.length(); i++) {
             if (Character.isLetter(a.charAt(i))) {
-                c += a.charAt(i);
+                c.append(a.charAt(i));
                 int dem = 1;
                 int so = 0;
 
-                while (Character.isDigit(a.charAt(i + dem)) && i + dem < a.length()) {
+                while (Character.isDigit(a.charAt(i + dem))) {
                     so = so * 10 + (int) (a.charAt(i + dem) - '0');
                     dem++;
+                    if(i + dem == a.length()-1) break;
                 }
 
                 for (int k = 0; k < so - 1; k++) {
-                    c += a.charAt(i);
+                    c.append(a.charAt(i));
                 }
                 i += dem - 1;
             }
         }
 
-        return c;
+        return c.toString();
     }
 }
