b24ea66ffc7419b6bfd05b419da0f2319b337d17
bkdnntdat <bkdn.ntdat@gmail.com>
2019-06-11 14:45:40 +0700

    Update\

--- src/main/java/String17.java
+++ src/main/java/String17.java
@@ -3,28 +3,26 @@ public class String17 {
     public String tinhHieu(String a, String b) {
         StringBuilder c = new StringBuilder("");
 
-        if (a.length() < b.length() || (a.length() == b.length() && a.compareTo(b) < 0)) return "-" + tinhHieu(b, a);
+        if (a.compareTo(b) < 0) return "-" + tinhHieu(b, a);
 
-        if (a.compareTo(b) > 0) {
-            a = new StringBuffer(a).reverse().toString();
-            b = new StringBuffer(b).reverse().toString();
-            while (b.length() < a.length()) b += "0";
-            int nho = 0;
+        a = new StringBuffer(a).reverse().toString();
+        b = new StringBuffer(b).reverse().toString();
+        while (b.length() < a.length()) b += "0";
+        int nho = 0;
 
-            for (int i = 0; i < b.length(); i++) {
-                int ia = a.charAt(i) - '0';
-                int ib = b.charAt(i) - '0' + nho;
-                int kq = ia - ib;
+        for (int i = 0; i < b.length(); i++) {
+            int ia = a.charAt(i) - '0';
+            int ib = b.charAt(i) - '0' + nho;
+            int kq = ia - ib;
 
-                if (kq < 0) {
-                    kq += 10;
-                    nho = 1;
-                } else {
-                    nho = 0;
-                }
-
-                c.append(kq);
+            if (kq < 0) {
+                kq += 10;
+                nho = 1;
+            } else {
+                nho = 0;
             }
+
+            c.append(kq);
         }
 
         return c.reverse().toString();
