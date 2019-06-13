25daa7ec3c08fdbaadd73816c9c641ededc5d8a3
bkdnntdat <50607363+bkdnntdat@users.noreply.github.com>
2019-06-13 09:42:36 +0700

    Update Triangle.java

--- src/main/java/OOP1/Triangle.java
+++ src/main/java/OOP1/Triangle.java
@@ -1,31 +1,22 @@
 package OOP1;
 
+import java.awt.geom.Point2D;
 import java.util.Arrays;
 
 public class Triangle {
     double x1, x2, x3, y1, y2, y3;
 
     public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.y1 = y1;
-        this.y2 = y2;
-        this.y3 = y3;
-    }
-
-    private double canh(double x1, double y1, double x2, double y2) {
-        double a = x1 - x2;
-        double b = y1 - y2;
-
-        return Math.sqrt(a * a + b * b);
+        this.P1.setLocation(x1, y1);
+        this.P2.setLocation(x2, y2);
+        this.P3.setLocation(x3, y3);
     }
 
     public String getType() {
         double c[] = new double[3];
-        c[0] = canh(x1, y1, x2, y2);
-        c[1] = canh(x1, y1, x3, y3);
-        c[2] = canh(x3, y3, x2, y2);
+        c[0] = P1.distance(P2);
+        c[1] = P2.distance(P3);
+        c[2] = P1.distance(P3);
         Arrays.sort(c);
 
         if (c[0] == c[2]) return "Deu";
@@ -44,18 +35,18 @@ public class Triangle {
 
     public double getPerimeter(){
         double c[] = new double[3];
-        c[0] = canh(x1, y1, x2, y2);
-        c[1] = canh(x1, y1, x3, y3);
-        c[2] = canh(x3, y3, x2, y2);
+        c[0] = P1.distance(P2);
+        c[1] = P2.distance(P3);
+        c[2] = P1.distance(P3);
 
         return c[1]+c[2]+c[0];
     }
 
     public double getArea(){
         double c[] = new double[3];
-        c[0] = canh(x1, y1, x2, y2);
-        c[1] = canh(x1, y1, x3, y3);
-        c[2] = canh(x3, y3, x2, y2);
+        c[0] = P1.distance(P2);
+        c[1] = P2.distance(P3);
+        c[2] = P1.distance(P3);
         double p = getPerimeter()/2;
 
         return Math.sqrt(p*(p-c[0])*(p-c[1])*(p-c[2]));
